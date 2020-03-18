391e51c3df6f001538fec99267b206a6b4da9318
Dmitriy Kovalev <kacevnik@yandex.ru>
2020-03-18 20:12:09 +0300

    added background for game level

--- src/Components/Canvas/Canvas.jsx
+++ src/Components/Canvas/Canvas.jsx
@@ -17,6 +17,10 @@ function Canvas({ level, game }) {
         if (level === 'select') {
             setBp('left top, right top, left bottom, right bottom');
         }
+
+        if (level === 'game') {
+            setBp('center bottom');
+        }
     }
 
     const style = {
